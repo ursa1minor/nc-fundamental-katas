42d082f94063872de8fd6e27582353a80675cee7
tom <tom@michaels-MacBook-Pro.local>
2022-08-14 08:09:02 +0100

    Initial tests pass

--- change-calculator/change-calculator.js
+++ change-calculator/change-calculator.js
@@ -1,6 +1,36 @@
-// Please do not change the name of this function
+// function that calculates the coins you would use to 
+// give a customer their change in a shop. 
+// function takes one argument, which is the amount of change 
+// needed, in pence; should always return an object.
+
 function changeCalculator (num) {
-  // Your code here
+  const coins = {
+    '1': 0,
+    '2': 0,
+    '5': 0,
+    '10': 0,
+    '20': 0,
+    '50': 0,
+    '100': 0,
+    '200': 0,
 };
+  const change = {};
+
+
+if (num === 1) {coins['1'] = 1;}
+if (num === 2) {coins['2'] = 1;}
+
+for (let key in coins) {
+  if (coins[key] !== 0) {
+    change[key] = coins[key]; 
+console.log(key);
+console.log(coins[key]);
+console.log(change);
+  }
+}
+console.log(change);
+return change;
+  }
+
 
 module.exports = changeCalculator;
