f6436280f234554a59a76c23de856f2328df38f0
tom <tom@michaels-MacBook-Pro.local>
2022-08-17 20:43:34 +0100

    pigLatin passes 3 tests

--- __tests__/pig-latin.test.js
+++ __tests__/pig-latin.test.js
@@ -17,5 +17,11 @@ describe('pigLatin()', () => {
   test('empty string returns empty string', () => {
     expect(pigLatin("")).toBe("");
   });
+  test('string starts with consonant, returns string ending in first letter + ay', () => {
+    expect(pigLatin("northcoders")).toBe("orthcodersnay");
+  });
+  test('string starts with vowel, returns string ending in first letter + way', () => {
+    expect(pigLatin("algorithm")).toBe("algorithmway");
+  });
 
 });
--- pig-latin/pig-latin.js
+++ pig-latin/pig-latin.js
@@ -3,10 +3,16 @@
 // To translate to [Pig Latin](https://en.wikipedia.org/wiki/Pig_Latin) you take the first consonant (or consecutive consonants) of each word, move it to the end and also append 'ay'. If the word starts with a vowel, just append 'way' at the end.
 
 function pigLatin(str) {
-  if (str === "") {
-    return "";
-  }
- 
-};
+  if (str.length === 0) {
+    return "";}
+    console.log(str[0]);
+  
+ if (str.startsWith("a" || "e" || "i" || "o" || "u")) {
+    return (str + "way");
+}
+if (str.startsWith("a" || "e" || "i" || "o" || "u") === false) {
+  return (str.substring(1) + str[0] + "ay");
+} 
+}
 
 module.exports = pigLatin
