4872a939d07e9cc147224b70c12fb8476f3587a9
tom <tom@michaels-MacBook-Pro.local>
2022-08-18 00:44:39 +0100

    rotateArray passes tests

--- __tests__/rotate-array.test.js
+++ __tests__/rotate-array.test.js
@@ -1,30 +1,21 @@
 const rotateArray = require('../rotate-array/rotate-array.js');
 
-// - If number positive return the array rotated x times to the right
-
-
-//rotateArray([1, 2, 3], 1);
-// should return [3, 1, 2]
-
-//rotateArray([1, 2, 3, 4, 5], 3);
-// should return [3, 4, 5, 1, 2]
-
-// - If number negative return the array rotated x times to the left
-
-//rotateArray([1, 2, 3], -1);
-// should return [2, 3, 1]
-
-//rotateArray([1, 2, 3, 4, 5], -3);
-// should return [4, 5, 1, 2, 3]
-
-
 
 describe("rotateArray function tests", () => {
 	test("returns an array", () => {
 		expect(rotateArray([], 0)).toEqual([]);
 	});	
-	test("returns an array", () => {
+	test("returns array with same values when num 0", () => {
 		expect(rotateArray([1, 2, 3], 0)).toEqual([1, 2, 3]);
 	});	
-
+	test("returns an array with correctly changed values when num +ve", () => {
+		expect(rotateArray([1, 2, 3], 1)).toEqual([3, 1, 2]);
+		expect(rotateArray([1, 2, 3, 4, 5], 3)).toEqual([3, 4, 5, 1, 2]);
+	});	
+	test("returns an array with correctly changed values when num -ve", () => {
+		expect(rotateArray([1, 2, 3], -1)).toEqual([2, 3, 1]);
+		expect(rotateArray([1, 2, 3, 4, 5], 3)).toEqual([3, 4, 5, 1, 2]);
+	});	
 });
+
+
--- rotate-array/rotate-array.js
+++ rotate-array/rotate-array.js
@@ -2,12 +2,33 @@
 // takes 2 args, an array and a number
 // always returns an array
 
+
 function rotateArray (arr, num) {
 
-  if (arr.length === 0 || num === 0) {
-  return arr;
-  }
+  let newArr = arr;
 
-};
+    if (arr.length === 0 || num === 0) {
+    return newArr;
+    } else if (num > 0) {
+
+    for (let i = 0; i < num; i++) {
+  
+      const last = newArr.pop();
+      newArr.unshift(last);
+    }
+    return newArr;
+  
+  } else if (num < 0) {
+
+    for (let i = 0; i < -num; i++) {
+
+      const first = newArr.shift();
+      newArr.push(first);
+    }
+    return newArr;
+  }
+}
+
+
 
 module.exports =  rotateArray;
