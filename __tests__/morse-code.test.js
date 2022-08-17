5a37913ea0566f79900bf1066b78c37fdfea900f
tom <tom@michaels-MacBook-Pro.local>
2022-08-17 22:09:51 +0100

    morseCode passes 2 tests

--- __tests__/morse-code.test.js
+++ __tests__/morse-code.test.js
@@ -1,43 +1,3 @@
-// ## Morse Code Table
-
-// | Letter | Code  |
-// | ------ | ----- |
-// | A      | .-    |
-// | B      | -...  |
-// | C      | -.-.  |
-// | D      | -..   |
-// | E      | .     |
-// | F      | ..-.  |
-// | G      | --.   |
-// | H      | ....  |
-// | I      | ..    |
-// | J      | .---  |
-// | K      | -.-   |
-// | L      | .-..  |
-// | M      | --    |
-// | N      | -.    |
-// | O      | ---   |
-// | P      | .--.  |
-// | Q      | --.-  |
-// | R      | .-.   |
-// | S      | ...   |
-// | T      | -     |
-// | U      | ..-   |
-// | V      | ...-  |
-// | W      | .--   |
-// | X      | -..-  |
-// | Y      | -.--  |
-// | Z      | --..  |
-// | 0      | ----- |
-// | 1      | .---- |
-// | 2      | ..--- |
-// | 3      | ...-- |
-// | 4      | ....- |
-// | 5      | ..... |
-// | 6      | -.... |
-// | 7      | --... |
-// | 8      | ---.. |
-// | 9      | ----. |
 
 
 //morseDecode(".... ..");
@@ -56,12 +16,18 @@
 
 
 
-const morseCode = require('../morse-code/morse-code.js');
+const {morseCode, decodeMorseChar} = require('../morse-code/morse-code.js');
 
 describe('morseCode()', () => {
   test('empty string returns empty string', () => {
     expect(morseCode("")).toBe("");
   });
+  test('single character is output correctly', () => {
+    expect(morseCode("....")).toBe("H");
+  });
+//   test('HI is output correctly from morse code H and I', () => {
+//     expect(morseCode(".... ..")).toBe("HI");
+//   });
   
 
 });
--- morse-code/morse-code.js
+++ morse-code/morse-code.js
@@ -1,10 +1,69 @@
 // The challenge is to implement a function which decodes a morse code string!
 // The morse code will have a space between each letter and three between each word
 
+const morse = {
+  A : ".-",
+  B : "-...", 
+  C : "-.-.",  
+  D : "-..",   
+  E : ".",    
+  F : "..-.",  
+  G : "--.",   
+  H : "....",  
+  I : "..",    
+  J : ".---",  
+  K : "-.-",   
+  L : ".-..",  
+  M : "--",    
+  N : "-.",    
+  O : "---",  
+  P : ".--.",  
+  Q : "--.-",  
+  R : ".-.",   
+  S : "...",   
+  T : "-",     
+  U : "..-",   
+  V : "...-",  
+  W : ".--",  
+  X : "-..-",  
+  Y : "-.--", 
+  Z : "--..", 
+  0 : "-----", 
+  1 : ".----",
+  2 : "..---", 
+  3 : "...--", 
+  4 : "....-", 
+  5 : ".....", 
+  6 : "-....", 
+  7 : "--...", 
+  8 : "---..", 
+  9 : "----." }
+
+function decodeMorseChar (str) {
+  const morseArray = Object.values(morse);
+  const charArray = Object.keys(morse);
+  
+  const n = morseArray.findIndex(str);
+  return charArray[n];
+
+  }
+
+
 function morseCode (str) {
   if (str.length === 0) {
     return "";
+  } else {
+    const morseArray = Object.values(morse);
+    const charArray = Object.keys(morse);
+    //console.log(morseArray)
+    //console.log(charArray)
+    
+    if (morseArray.includes(str)) {
+      const i = morseArray.indexOf(str);
+      return charArray[i];
+    }
+  
   }
 };
 
-module.exports = morseCode;
+module.exports = {morseCode, decodeMorseChar};
