759d9bdcb883c1143691aafb43b708f77de2f4ab
tom <tom@michaels-MacBook-Pro.local>
2022-08-17 23:05:49 +0100

    pigLatin passes 5 tests

--- pig-latin/pig-latin.js
+++ pig-latin/pig-latin.js
@@ -3,19 +3,19 @@
 // To translate to [Pig Latin](https://en.wikipedia.org/wiki/Pig_Latin) you take the first consonant (or consecutive consonants) of each word, move it to the end and also append 'ay'. If the word starts with a vowel, just append 'way' at the end.
 
 function pigLatin(str) {
-  if (str.length === 0) {
+  if (str.length === 0) {     
     return "";}
   
-  if (str.includes(" ")) {
+  if (str.includes(" ")) {    //sentence
     const words = str.split(" "); //array
-    const pigWords = words.map(word => littlePig(word)); //array
+    const pigWords = words.map(word => pigWord(word)); //array
     return pigWords.join(" ");
 
-  } else {
-  return littlePig(str);
+  } else {                    //single word
+  return pigWord(str);
 }}
 
-function littlePig(str) {
+function pigWord(str) {     
   if (str.startsWith("a") || str.startsWith("e") || str.startsWith("i") || str.startsWith("o") || str.startsWith("u")) {
     return (str + "way");
   }
