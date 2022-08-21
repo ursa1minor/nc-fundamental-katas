02374a82865ec5c77cfbc824233a251f382958f9
tom <tom@michaels-MacBook-Pro.local>
2022-08-21 11:23:13 +0100

    herdTheBabies 2 tests pass

--- __tests__/herd-the-babies.test.js
+++ __tests__/herd-the-babies.test.js
@@ -1,8 +1,10 @@
 const herdTheBabies = require('../herd-the-babies/herd-the-babies.js');
 
 describe("herdTheBabies function tests", () => {
-	test("", () => {
+	test.only("should return empty string when passed empty string", () => {
 		expect(herdTheBabies("")).toBe("");
 	});	
-
+	test.only("should return Aa when passed aA", () => {
+		expect(herdTheBabies("aA")).toBe("Aa");
+	});	
 });
--- herd-the-babies/herd-the-babies.js
+++ herd-the-babies/herd-the-babies.js
@@ -1,7 +1,26 @@
 function herdTheBabies (str) {
 
-  return str;
+  if (str === "") {
+    return str;
+  } else {
 
+  const charCodeArray = [];
+
+  for (let i = 0; i < str.length; i++) {
+    charCodeArray.push(str.charCodeAt(i))
+  }
+  charCodeArray.sort();
+
+  console.log(charCodeArray);
+
+  const charArray = charCodeArray.map(x => String.fromCharCode(x));
+
+  console.log(charArray);
+
+  const newStr = charArray.join("")
+
+  return newStr;
+};
 };
 
 module.exports = herdTheBabies;
